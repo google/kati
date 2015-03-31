cc9f580a12cc21bbb216bbe9c3cab94593c3163f
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-03-31 17:40:44 +0900

    add canned_recipes.mk
    % make
    echo test
    test
    % ../../kati
    *kati*: define run-echo
    *kati*: echo $@
    *kati*: endef
    *kati*: test:
    *kati*:         $(run-echo)
    Makefile:6: *** missing separator.
    %

---
+++ test/canned_recipes.mk
@@ -0,0 +1,11 @@
+# TODO
+# http://www.gnu.org/software/make/manual/make.html#Canned-Recipes
+
+# canned recipes are used in gyp-generated Makefile (fixup_dep etc)
+
+define run-echo
+echo $@
+endef
+
+test:
+	$(run-echo)
