50e1aef78d5e3e195060da7a3e0690bb839e62a4
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-03-31 10:35:25 +0900

    add subst.mk
    need to implement subst func, and maybe fix evalExprSlice?
    (split func arg with , before expanding vars)
    
    % make
    echo a,b,c
    a,b,c
    % ../../kati
    *kati*: parseAssign comma:= , :=
    *kati*: parseAssign empty:= :=
    *kati*: parseAssign space:= $(empty) $(empty) :=
    *kati*: parseAssign foo:= a b c :=
    *kati*: parseAssign bar:= $(subst $(space),$(comma),$(foo)) :=
    *kati*: comma=,
    *kati*: empty=
    *kati*: space=$(empty) $(empty)
    *kati*: foo=a b c
    *kati*: bar=$(subst $(space),$(comma),$(foo))
    *kati*: test:
    *kati*:         echo $(bar)
    *kati*: ASSIGN: comma=,
    *kati*: ASSIGN: empty=
    *kati*: ASSIGN: space=
    *kati*: ASSIGN: foo=a b c
    *kati*: ASSIGN: bar=
    *kati*: RULE: test=
    *kati*: Building: test
    echo

---
+++ test/subst.mk
@@ -0,0 +1,12 @@
+# TODO
+
+# http://www.gnu.org/software/make/manual/make.html#Syntax-of-Functions
+comma:= ,
+empty:=
+space:= $(empty) $(empty)
+foo:= a b c
+bar:= $(subst $(space),$(comma),$(foo))
+# bar is now `a,b,c'
+
+test:
+	echo $(bar)
