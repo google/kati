b4714de7f010c65b7ce271ca5459fae4216509d5
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-02 00:06:58 +0900

    Add test/semi_in_var.mk for the record

---
+++ test/semi_in_var.mk
@@ -0,0 +1,17 @@
+# TODO: Not sure how this behavior can be explained. We probably will
+# not need to support bar and baz, but we probably need foo.
+
+ECHO=@echo $@
+SEMI=;
+RULE=bar: ; $(EHCO)
+
+all: foo bar baz
+
+foo: ; $(ECHO)_1
+	$(ECHO)_2
+
+$(RULE)
+	$(ECHO)_2
+
+baz: $(SEMI) @echo $@_1
+	@echo $@_2
