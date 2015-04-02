834b2256d7941ec29bc318e53c280754f207ab04
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-02 16:05:30 +0900

    fix var_eval.mk (exect in comment)
    add one more test in var_eval
    D=$$
    O=(
    C=)
    $D$Obar$c will be $(bar)

--- test/var_eval.mk
+++ test/var_eval.mk
@@ -3,16 +3,22 @@ var2 = $$(bar)
 var3 := $($(bar))
 var4 := $$(bar)
 
+D=$$
+O=(
+C=)
+
 # expects
 #  foo
-#  $bar
+#  $(bar)
 #
-#  $bar
+#  $(bar)
+#  $(bar)
 test:
 	echo '$(var1)'
 	echo '$(var2)'
 	echo '$(var3)'
 	echo '$(var4)'
+	echo '$D$Obar$C'
 
 bar = foo
 foo = foo
