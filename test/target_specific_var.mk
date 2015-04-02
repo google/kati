39f8167da98cfff16cc08d8b9ef12ee774837b26
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-02 09:45:16 +0900

    add target_specific_var.mk
    out.make
    
    === test ===
    touch prog.c
    echo cc -g -o prog.o -c prog.c
    cc -g -o prog.o -c prog.c
    echo prog -g
    prog -g
    
    === FILES ===
    prog.c
    
    out.kati
    
    === test ===
    touch prog.c
    echo cc -O -o prog.o -c prog.c
    cc -O -o prog.o -c prog.c
    panic: no rule to make target "CFLAGS"
    
    goroutine 1 [running]:
    main.main()
    	/usr/local/google/home/ukai/src/kati/main.go:40 +0x3d5
    
    === FILES ===
    prog.c

---
+++ test/target_specific_var.mk
@@ -0,0 +1,18 @@
+# TODO
+# https://www.gnu.org/software/make/manual/html_node/Target_002dspecific.html
+
+CFLAGS = -O
+
+test: prog
+
+prog: CFLAGS = -g
+prog : prog.o
+	echo prog $(CFLAGS)
+
+prog.o : prog.c
+	echo cc $(CFLAGS) -o prog.o -c prog.c
+
+prog.c:
+	touch prog.c
+
+
