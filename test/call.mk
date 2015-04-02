8310a4f9fe6981790ee2d5efdd29001084dce45e
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-02 11:47:30 +0900

    add call.mk

---
+++ test/call.mk
@@ -0,0 +1,18 @@
+# TODO
+
+# from gyp-generated Makefile
+empty :=
+space := $(empty) $(empty)
+
+replace_spaces = $(subst $(space),?,$1)
+unreplace_spaces = $(subst ?,$(space),$1)
+dirx = $(call unreplace_spaces,$(dir $(call replace_spaces,$1)))
+
+test: foo
+	echo $(call dirx,foo/bar)
+	echo $(call dirx,foo bar/baz quux)
+	echo $(call dirx,foo,bar)
+
+foo:
+	mkdir foo "foo bar"
+
