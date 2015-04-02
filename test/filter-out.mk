ab00ce8b78c17e0f8fd452c902cb3de3df50c457
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-02 23:58:01 +0900

    add filter.mk and filter-out.mk

---
+++ test/filter-out.mk
@@ -0,0 +1,7 @@
+# TODO
+objects=main1.o foo.o main2.o bar.o
+mains=main1.o main2.o
+
+# expect a list which contains all the object files not in `mains'
+test:
+	echo $(filter-out $(mains),$(objects))
---
+++ test/filter.mk
@@ -0,0 +1,5 @@
+# TODO
+sources := foo.c bar.c baz.s ugh.h
+
+test:
+	echo cc $(filter %.c %.s,$(sources)) -o foo
