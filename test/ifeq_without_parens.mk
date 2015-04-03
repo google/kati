d99b7bcd2ad5d2d4817d4273b88613bdc56d2091
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-03 14:35:14 +0900

    Add test/ifeq_without_parens.mk
    I forgot to add this.

---
+++ test/ifeq_without_parens.mk
@@ -0,0 +1,42 @@
+VAR=var
+VARREF=VAR
+EMPTY=
+UNDEFREF=UNDEFINED
+
+RESULT=
+
+ifeq "$(VAR)" "var"
+RESULT += PASS
+else
+RESULT += FAIL
+endif
+ifneq 	 "$(VAR)"  "var" 
+RESULT += FAIL
+else
+RESULT += PASS
+endif
+
+ifeq '$(VAR)' "var"
+RESULT += PASS
+else
+RESULT += FAIL
+endif
+ifeq "$(VAR)" 'var'
+RESULT += PASS
+else
+RESULT += FAIL
+endif
+
+ifeq "$(UNDEFINED)" ""
+RESULT += PASS
+else
+RESULT += FAIL
+endif
+ifeq "" "$(UNDEFINED)"
+RESULT += PASS
+else
+RESULT += FAIL
+endif
+
+test:
+	echo $(RESULT)
