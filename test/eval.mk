10c0106119ad17b3f9bb9df724cfef0a7ac9bd03
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-03 16:14:32 +0900

    Add test/eval.mk

---
+++ test/eval.mk
@@ -0,0 +1,33 @@
+# TODO(hamaji)
+
+test1:
+	touch server.c server_priv.c server_access.c
+	touch client.c client_api.c client_mem.c
+
+test2: all
+
+PROGRAMS    = server client
+
+server_OBJS = server.o server_priv.o server_access.o
+server_LIBS = priv protocol
+
+client_OBJS = client.o client_api.o client_mem.o
+client_LIBS = protocol
+
+# Everything after this is generic
+
+.PHONY: all
+all: $(PROGRAMS)
+
+define PROGRAM_template =
+ $(1): $$($(1)_OBJS) $$($(1)_LIBS:%=-l%)
+ ALL_OBJS += $$($(1)_OBJS)
+endef
+
+$(foreach prog,$(PROGRAMS),$(eval $(call PROGRAM_template,$(prog))))
+
+$(PROGRAMS):
+	$(LINK.o) $^ $(LDLIBS) -o $@
+
+clean:
+	rm -f $(ALL_OBJS) $(PROGRAMS)
