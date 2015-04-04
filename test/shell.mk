a9e9299c262fa81c6eddb908505e87bca0d128c4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-04 18:36:43 +0900

    Remove dependency to SHELL from shell.mk

--- test/shell.mk
+++ test/shell.mk
@@ -1,12 +1,12 @@
 test:
 	echo $(shell pwd)
 	echo $(shell false)
-	echo $(shell  echo -e "\na \n  b 	\n " )
-	echo $(shell  echo -e "\na \n  b 	\n " )X
-	echo X$(shell  echo -e "\n\n" )Y
-	echo X$(shell  echo -e "a\n\n" )Y
-	echo X$(shell  echo -e "\n\nb" )Y
-	echo X$(shell  echo -e "\n\nb" )Y
-	echo X$(shell  echo -e "\n\n\nb" )Y
-	echo X$(shell  echo -e "   b" )Y
-	echo X$(shell  echo -e "b   " )Y
+	echo $(shell  /bin/echo -e "\na \n  b 	\n " )
+	echo $(shell  /bin/echo -e "\na \n  b 	\n " )X
+	echo X$(shell  /bin/echo -e "\n\n" )Y
+	echo X$(shell  /bin/echo -e "a\n\n" )Y
+	echo X$(shell  /bin/echo -e "\n\nb" )Y
+	echo X$(shell  /bin/echo -e "\n\nb" )Y
+	echo X$(shell  /bin/echo -e "\n\n\nb" )Y
+	echo X$(shell  /bin/echo -e "   b" )Y
+	echo X$(shell  /bin/echo -e "b   " )Y
