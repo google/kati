47a98c12ddff9240671793a44406ac9540053924
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-06 10:10:11 +0900

    add implicit_pattern_rule_for_no_commands.mk
    If none of the explicit rules for a target has commands, then `make'
    searches for an applicable implicit rule to find some commands *note
    Using Implicit Rules: Implicit Rules.).

---
+++ test/implicit_pattern_rule_for_no_commands.mk
@@ -0,0 +1,16 @@
+# TODO
+test: foo.c foo.h
+
+foo.c foo.h:
+	touch $@
+
+test2: foo
+
+CFLAGS=-O
+foo: foo.o
+	echo cc $(CFLAGS) -o $@ $<
+foo.o: CFLAGS=-g
+foo.o: foo.h
+
+%.o: %.c
+	echo cc $(CFLAGS) -o $@ -c $<
