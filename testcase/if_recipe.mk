3c445a8ca14bef4fa0c3c028c72d60c5947802fe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-07 05:34:01 +0900

    Add testcase/if_recipe.mk

---
+++ testcase/if_recipe.mk
@@ -0,0 +1,26 @@
+# TODO(hamaji): fix
+
+test1:
+	echo TEST
+ifdef UNDEFINED
+	echo FAIL
+else
+	echo PASS
+endif
+	echo DONE
+
+test2:
+ifdef UNDEFINED
+	echo FAIL
+else
+	echo PASS
+endif
+	echo DONE
+
+test3:
+ifndef UNDEFINED
+	echo PASS
+else
+	echo FAIL
+endif
+	echo DONE
