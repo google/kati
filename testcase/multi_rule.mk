dc1049fcd26441ae6ce4320bdf233769d3eb16e7
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-07 00:08:05 +0900

    add multi_rule

---
+++ testcase/multi_rule.mk
@@ -0,0 +1,15 @@
+# TODO
+test: foo.c
+
+# simplified case for gyp-generated action targets
+# with 'process_outputs_as_sources': 1
+# and 'hard_dependency': 1
+foo.c: CFLAGS:=-g
+foo.c:
+	echo generating foo.c
+
+outputs := foo.c
+
+$(outputs): |
+
+
