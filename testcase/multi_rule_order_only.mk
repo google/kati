296ae70afe4f2aa3c164016571c5a58b34f824ad
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-07 10:32:37 +0900

    add multi_rule_order_only

---
+++ testcase/multi_rule_order_only.mk
@@ -0,0 +1,28 @@
+# TODO
+test: foo.c
+
+foo.c:
+	touch foo.c
+
+OBJS := foo.o
+
+$(OBJS): | bar.a 
+
+$(OBJS): CFLAGS:=-g
+
+%.o: %.c FORCE_DO_CMD
+	echo compile $@ from $<
+
+.PHONY: FORCE_DO_CMD
+FORCE_DO_CMD:
+
+bar.a:
+	echo archive $@
+
+foo.a: $(OBJS)
+	echo archive $@
+
+test2: foo.a
+
+
+
