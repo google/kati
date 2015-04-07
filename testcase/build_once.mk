c43c44bee03efcc24409d1a9a71044e106ec8bd1
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-07 11:17:31 +0900

    add build_once

---
+++ testcase/build_once.mk
@@ -0,0 +1,31 @@
+# TODO
+# expect protoc compile/link only once.
+test: foo
+
+foo: foo.o bar.o
+	echo link $@ from $<
+
+%.o: %.c FORCE_DO_CMD
+	echo compile $@ from $<
+
+.PHONY: FORCE_DO_CMD
+FORCE_DO_CMD:
+
+foo.c: | protoc
+
+foo.c: foo.proto
+	echo protoc $@ from $<
+
+foo.proto:
+
+bar.c: | protoc
+
+bar.c: bar.proto
+	echo protoc $@ from $<
+
+bar.proto:
+
+protoc: proto.o
+	echo link $@ from $<
+
+proto.c:
