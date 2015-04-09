58ed08322bc254f4e8d74b892ecea3d4a4257f39
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-09 09:43:28 +0900

    add empty and undefined case for var_append

--- testcase/var_append.mk
+++ testcase/var_append.mk
@@ -1,16 +1,27 @@
 S:=simple
 R=recursive
+SE:=
+RE=
 
 foo=FOO
 bar=
 
 S+=$(foo) $(bar)
 R+=$(foo) $(bar)
+SE+=$(foo) $(bar)
+RE+=$(foo) $(bar)
+U+=$(foo) $(bar)
 
 bar=BAR
 
 test:
 	echo "$(S)"
 	echo "$(R)"
+	echo "$(SE)"
+	echo "$(RE)"
+	echo "$(U)"
 	echo "$(flavor S)"
 	echo "$(flavor R)"
+	echo "$(flavor SE)"
+	echo "$(flavor RE)"
+	echo "$(flavor U)"
