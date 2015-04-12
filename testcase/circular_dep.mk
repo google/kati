bc875dfd38ac4036b4a77a8c5d85602e927fadb8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-13 01:08:43 +0900

    Always update ex.done

---
+++ testcase/circular_dep.mk
@@ -0,0 +1,18 @@
+test: self loop not_circular1 not_circular2
+	echo PASS
+
+self: self
+	echo $@
+
+loop: loop1
+	echo $@
+
+loop1: loop2
+	echo $@
+
+loop2: loop
+	echo $@
+
+not_circular1: Makefile
+
+not_circular2: Makefile
