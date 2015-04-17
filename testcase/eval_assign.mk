030736c8bb0dfeed8e3dd42651c65dfd1f344b3a
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-17 11:45:20 +0900

    fix $(eval foo := $(bar)) case
    if bar is "$(baz)" or so, it needs to eval when assigned.

--- testcase/eval_assign.mk
+++ testcase/eval_assign.mk
@@ -1,12 +1,18 @@
 bar := FAIL
 pb := prog: bar
 $(pb) := PASS
+
+moge := PASS
+hoge := $$(moge)
+
 define evaltest
  $(eval foo := PASS)
  $(eval bar := $$(foo))
  echo $(bar)
  $(eval prog: bar := PASS)
  echo $($(pb))
+ $(eval fuga := $(hoge))
+ echo $(fuga)
 endef
 
 test:
