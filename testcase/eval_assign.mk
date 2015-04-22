b4c5a2fb667833045cde645acf2a0d47b0558ea6
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-22 10:46:49 +0900

    fix: handle $(eval foo := $(var)) case

--- testcase/eval_assign.mk
+++ testcase/eval_assign.mk
@@ -1,18 +1,30 @@
 bar := FAIL
-pb := prog: bar
-$(pb) := PASS
+pf := prog: baz
+$(pf) := PASS
 
 moge := PASS
 hoge := $$(moge)
+
+a := FAIL
+b := c
+c := PASS
+
+d := FAIL
+e := $$f
+f := PASS
 
 define evaltest
  $(eval foo := PASS)
  $(eval bar := $$(foo))
  echo $(bar)
- $(eval prog: bar := PASS)
- echo $($(pb))
+ $(eval prog: baz := FAIL)
+ echo $($(pf))
  $(eval fuga := $(hoge))
  echo $(fuga)
+ $(eval a := $($(b)))
+ echo $(a)
+ $(eval d := $(e))
+ echo $(d)
 endef
 
 test:
