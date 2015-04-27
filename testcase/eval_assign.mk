5fbd0bdf4551af6b9233a0d52e1138cfba882a93
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-27 22:35:03 +0900

    Revert "fix: handle $(eval foo := $(var)) case"
    This reverts commit ca9183b18f7445419d67913328f605a0ec5e0288.
    
    Repro step:
    
    $ ./repo/android.sh time kati -n
    ...
    including ./system/vold/Android.mk ...
    including ./tools/external/fat32lib/Android.mk ...
    build/core/prebuilt_internal.mk:258: warning: overriding commands for target "javalib.jar"
    build/core/prebuilt_internal.mk:0: warning: ignoring old commands for target "javalib.jar"
    build/core/prebuilt_internal.mk:258: warning: overriding commands for target "javalib.jar"
    build/core/prebuilt_internal.mk:0: warning: ignoring old commands for target "javalib.jar"
    ...

--- testcase/eval_assign.mk
+++ testcase/eval_assign.mk
@@ -1,30 +1,18 @@
 bar := FAIL
-pf := prog: baz
-$(pf) := PASS
+pb := prog: bar
+$(pb) := PASS
 
 moge := PASS
 hoge := $$(moge)
-
-a := FAIL
-b := c
-c := PASS
-
-d := FAIL
-e := $$f
-f := PASS
 
 define evaltest
  $(eval foo := PASS)
  $(eval bar := $$(foo))
  echo $(bar)
- $(eval prog: baz := FAIL)
- echo $($(pf))
+ $(eval prog: bar := PASS)
+ echo $($(pb))
  $(eval fuga := $(hoge))
  echo $(fuga)
- $(eval a := $($(b)))
- echo $(a)
- $(eval d := $(e))
- echo $(d)
 endef
 
 test:
