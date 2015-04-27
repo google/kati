d8cec17d195a8149bd3c7ca913e237e9d9e8bca8
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-04-28 00:08:09 +0900

    Revert "Revert "fix: handle $(eval foo := $(var)) case""
    This reverts commit 5fbd0bdf4551af6b9233a0d52e1138cfba882a93.
    
    rhs should trim left after 1st eval

--- testcase/eval_assign.mk
+++ testcase/eval_assign.mk
@@ -1,18 +1,36 @@
 bar := FAIL
-pb := prog: bar
-$(pb) := PASS
+pf := prog: baz
+$(pf) := PASS
 
 moge := PASS
 hoge := $$(moge)
+
+a := FAIL
+b := c
+c := PASS
+
+d := FAIL
+e := $$f
+f := PASS
+
+empty:=#
+g := FAIL
+h := $(empty) PASS
 
 define evaltest
  $(eval foo := PASS)
  $(eval bar := $$(foo))
  echo $(bar)
- $(eval prog: bar := PASS)
- echo $($(pb))
+ $(eval prog: baz := FAIL)
+ echo $($(pf))
  $(eval fuga := $(hoge))
  echo $(fuga)
+ $(eval a := $($(b)))
+ echo $(a)
+ $(eval d := $(e))
+ echo $(d)
+ $(eval g := $(h))
+ echo _$(g)_
 endef
 
 test:
