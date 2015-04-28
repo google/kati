b4240ee0c1c81131a697f22e7ce63b65af88fefa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-04-28 22:31:34 +0900

    Fix var_with_space.mk for make4

--- testcase/var_with_space.mk
+++ testcase/var_with_space.mk
@@ -1,6 +1,14 @@
+MAKEVER:=$(shell make --version | ruby -n0e 'puts $$_[/Make (\d)/,1]')
+
+ifeq ($(MAKEVER),4)
+# A variable name with space is invalid on GNU make 4.
+all:
+	echo PASS
+else
 varname_with_ws:=hello, world!
 $(varname_with_ws):=PASS
 foo bar = PASS2
 all:
 	echo $(hello, world!)
 	echo $(foo bar)
+endif
