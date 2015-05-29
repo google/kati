9eb1cfe6834c1b94aae7d06e14780cd494f3dad7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-05-29 14:23:30 +0900

    Let export/unexport modify environment variables

--- testcase/export.mk
+++ testcase/export.mk
@@ -1,13 +1,25 @@
-# TODO: Fix
-
-export FOO = PASS
-BAR := PASS
+export FOO = PASS_FOO
+BAR := PASS_BAR
 export BAR
+export X Y Z
+X := PASS_X
+Y := PASS_Y
+Z := PASS_Z
 
 export BAZ = FAIL
 unexport BAZ
+
+unexport Y
+export Y X
+
+# GNU make 3 and 4 behave differently for this, but it must not mess
+# up FOO, BAR, X, Y, and Z.
+export FOO BAR X Y Z := FAIL
 
 test:
 	echo $$FOO
 	echo $$BAR
 	echo $$BAZ
+	echo $$X
+	echo $$Y
+	echo $$Z
