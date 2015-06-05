b978e11fe63e4e794b459e55d4cc25182f934411
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-05 11:02:45 +0900

    Fix $(strip). We need to strip whitespaces between words

--- testcase/strip.mk
+++ testcase/strip.mk
@@ -2,13 +2,19 @@ XY:=x 	y
 X:=$(subst y, ,$(XY))
 Y:=$(subst x, ,$(XY))
 
+define func
+foo
+bar
+endef
+
 test:
 	echo $(X)
 	echo $(Y)
 	echo $(strip $(X))
 	echo $(strip $(Y))
 	echo $(strip $(Y),$(X))
+	echo $(strip $(XY))
+	$(info $(strip $(call func)))
 
-# TODO: Hard to tell why. Fix this.
-#test2:
-#	echo $(strip $(X),$(Y))
+test2:
+	echo $(strip $(X),$(Y))
