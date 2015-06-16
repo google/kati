f9b6ec7293c79ba2a39f66ee336b073cb5309447
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-06-16 14:51:03 +0900

    add make-c to measure time to parse android makefiles.
    % time ./repo/android.sh make-c.sh > /tmp/make.log
    ./repo/android.sh make-c.sh > /tmp/make.log  24.43s user 11.21s system 101% cpu 35.116 total
    
    % tail -1 /tmp/make.log
    make -c: 34.680252934s
    
    % time ./repo/android.sh kati -c -find_cache_prunes=".git .repo out" > /tmp/kati.log
    fatal: Not a git repository: 'packages/apps/Camera2/.git'
    ./repo/android.sh kati -c -find_cache_prunes=".git .repo out" > /tmp/kati.log  35.68s user 17.44s system 177% cpu 29.982 total

---
+++ make-c.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Copyright 2015 Google Inc. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# simple tool to measure time to parse Makefiles in android.
+#
+# usage:
+#  $ ./repo/android.sh make-c.sh
+dir=$(cd $(dirname $0); pwd)
+go run ${dir}/make-c/main.go
