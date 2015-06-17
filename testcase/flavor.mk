d9e3a9285c248c0494f79f309e6b967b8c24a59c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-18 07:11:54 +0900

    Add a couple of failing testcases to origin and flavor

--- testcase/flavor.mk
+++ testcase/flavor.mk
@@ -4,11 +4,14 @@ A=a
 B:=b
 C+=c
 D?=d
+AREF:=A
 
 all:
 	echo $(flavor A) $(flavor B) $(flavor C) $(flavor D) $(flavor E)
 	echo $(flavor PATH)
 	echo $(flavor MAKEFILE_LIST)
+# TODO: Fix
+#	echo $(flavor $(AREF))
 
 # For some reason, $(flavor MAKECMDGOALS) should be "undefined"
 # echo $(flavor MAKECMDGOALS)
--- testcase/origin.mk
+++ testcase/origin.mk
@@ -3,6 +3,7 @@ FOO_SPACE_BAR:=foo bar
 FOO_COMMA_BAR:=foo,bar
 $(FOO_SPACE_BAR):=foo
 $(FOO_COMMA_BAR):=foo
+FOOREF := FOO
 
 test:
 	echo $(origin FOO)
@@ -12,6 +13,8 @@ test:
 	echo $(origin PATH)
 	echo $(origin MAKEFILE_LIST)
 	echo $(origin CC)
+# TODO: Fix
+#	echo $(origin $(FOOREF))
 
 # TODO: support environment override, command line, and override.
 # TODO: Also add more tests especially for += and ?=
