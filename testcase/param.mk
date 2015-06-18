e16c2b73e2fb6549b8988cb986ed77a96395c7b4
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-06-19 00:15:59 +0900

    add param.mk
    no need to assign/restore call parameter

---
+++ testcase/param.mk
@@ -0,0 +1,15 @@
+
+1:=foo
+$(info $(1) is foo)
+
+define param
+$(eval 1:=bar) param1-1=$(1) $(call param2,$(1))
+endef
+
+define param2
+param2-1=$(1)
+endef
+
+test:
+	@echo call param $(call param,baz)
+	@echo 1=$(1)
