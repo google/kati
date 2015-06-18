85ad631fd90796d26298cf0d24ec7803d53303b9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-18 15:37:40 +0900

    Add a testcase for value.mk

--- testcase/value.mk
+++ testcase/value.mk
@@ -5,8 +5,12 @@ FOO_COMMA_BAR:=foo,bar
 $(FOO_SPACE_BAR):=$PATH
 $(FOO_COMMA_BAR):=$PATH
 
+FOOREF := FOO
+
 test:
 	echo $(FOO)
 	echo $(value FOO)
 	echo $(value FOO BAR)
 	echo $(value FOO,BAR)
+# TODO: Fix
+#	echo $(value $(FOOREF))
