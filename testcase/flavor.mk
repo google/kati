c22fdb49c772760574b7b7969242e15dea30993d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-22 18:10:40 +0900

    [C++] Implement origin and flavor

--- testcase/flavor.mk
+++ testcase/flavor.mk
@@ -10,7 +10,7 @@ all:
 	echo $(flavor A) $(flavor B) $(flavor C) $(flavor D) $(flavor E)
 	echo $(flavor PATH)
 	echo $(flavor MAKEFILE_LIST)
-# TODO: Fix
+# TODO(go): Fix
 #	echo $(flavor $(AREF))
 
 # For some reason, $(flavor MAKECMDGOALS) should be "undefined"
