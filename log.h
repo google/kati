a6070abeca98b50323fbcb4b32fc9e848ba67312
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-22 16:51:04 +0900

    [C++] Fix envvar.mk

--- log.h
+++ log.h
@@ -6,30 +6,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LOG(args...) do {                       \
-    char log_buf[999];                          \
-    sprintf(log_buf, args);                     \
-    fprintf(stderr, "*kati*: %s\n", log_buf);   \
+#include "stringprintf.h"
+
+#define LOG(args...) do {                                           \
+    fprintf(stderr, "*kati*: %s\n", StringPrintf(args).c_str());    \
   } while(0)
 
-#define PERROR(...) do {                                        \
-    char log_buf[999];                                          \
-    sprintf(log_buf, __VA_ARGS__);                              \
-    fprintf(stderr, "%s: %s\n", log_buf, strerror(errno));      \
+#define PERROR(...) do {                                            \
+    fprintf(stderr, "%s: %s\n", StringPrintf(__VA_ARGS__).c_str(),  \
+            strerror(errno));                                       \
+    exit(1);                                                        \
+  } while (0)
+
+#define WARN(...) do {                                          \
+    fprintf(stderr, "%s\n", StringPrintf(__VA_ARGS__).c_str()); \
+  } while (0)
+
+#define ERROR(...) do {                                         \
+    fprintf(stderr, "%s\n", StringPrintf(__VA_ARGS__).c_str()); \
     exit(1);                                                    \
-  } while (0)
-
-#define WARN(...) do {                          \
-    char log_buf[999];                          \
-    sprintf(log_buf, __VA_ARGS__);              \
-    fprintf(stderr, "%s\n", log_buf);           \
-  } while (0)
-
-#define ERROR(...) do {                         \
-    char log_buf[999];                          \
-    sprintf(log_buf, __VA_ARGS__);              \
-    fprintf(stderr, "%s\n", log_buf);           \
-    exit(1);                                    \
   } while (0)
 
 #define CHECK(c) if (!(c)) ERROR("%s:%d: %s", __FILE__, __LINE__, #c)
