24d7a4abad957537c1e9cd32185ab5f53b1016c4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-30 02:27:52 +0900

    [C++] Fix wildcard.mk for GNU make 4

--- testcase/wildcard.mk
+++ testcase/wildcard.mk
@@ -1,9 +1,15 @@
+MAKEVER:=$(shell make --version | ruby -n0e 'puts $$_[/Make (\d)/,1]')
+
 files = $(wildcard M*)
 
 $(shell mkdir -p tmp)
 files += $(wildcard tmp/../M*)
 files += $(wildcard not_exist/../M*)
-files += $(wildcard tmp/../M* not_exist/../M* tmp/../M* [ABC] C B A)
+files += $(wildcard tmp/../M* not_exist/../M* tmp/../M*)
+# GNU make 4 does not sort the result of $(wildcard)
+ifeq ($(MAKEVER),3)
+files += $(wildcard [ABC] C B A)
+endif
 
 test1:
 	touch A C B
