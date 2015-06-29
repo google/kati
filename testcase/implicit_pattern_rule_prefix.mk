3ef3af3e79e7920e28ee0c2df2a70830756ffe48
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-06-29 16:05:44 +0900

    [C++] Fix implicit_pattern_rule_prefix.mk

---
+++ testcase/implicit_pattern_rule_prefix.mk
@@ -0,0 +1,17 @@
+# TODO(go): Fix
+
+MAKEVER:=$(shell make --version | ruby -n0e 'puts $$_[/Make (\d)/,1]')
+
+test: abcd
+
+abcd:
+
+# GNU make 3 does not prioritize the rule with a shortest stem.
+ifeq ($(MAKEVER),4)
+a%:
+	echo FAIL
+endif
+abc%:
+	echo PASS
+ab%:
+	echo FAIL
