dcea62bdb260c06021878057681e1eed51926a26
Fumitoshi Ukai <fumitoshi.ukai@gmail.com>
2015-07-03 11:41:05 +0900

    add unmatched_paren2.mk

---
+++ testcase/unmatched_paren2.mk
@@ -0,0 +1,16 @@
+# TODO
+# do we need to support this?
+
+foo = FOO
+bar = BAR
+dp := $$(
+$(dp)foo := PASS_UNMATCHED
+FOO1BAR := PASS_MATCHED
+baz = 0$($(foo)1$(bar)2
+# baz will be 0PASS_UNMATCHED, 1$(bar)2 will be discarded??
+baz2 = 0$($(foo)1$(bar))2
+# baz2 will be 0PASS_MATCHED2.
+
+test:
+	echo "$(baz)"
+	echo "$(baz2)"
