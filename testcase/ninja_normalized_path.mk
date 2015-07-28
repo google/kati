ab722baab7e46136b0df28e68e247a38f8af574e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-07-28 13:37:50 +0900

    Add ninja_normalized_path.mk

---
+++ testcase/ninja_normalized_path.mk
@@ -0,0 +1,13 @@
+# TODO(ninja): This is tough to fix with ninja. Ninja normalizes
+# target names while make does not.
+
+test1:
+	mkdir a b
+
+test2: a/b a/../a/b a/./b b/a b/../b/a b/./a
+
+a/%:
+	touch $@
+
+b/%:
+	echo $@
