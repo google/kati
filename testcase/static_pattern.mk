3f86300472a694b6afef6f93cc9a3787b3827ac2
Fumitoshi Ukai <ukai@google.com>
2015-07-29 13:47:55 +0900

    add testcase/static_pattern.mk

---
+++ testcase/static_pattern.mk
@@ -0,0 +1,16 @@
+# TODO(c): Fix
+srcs := a.cc b.cc c.cc
+srcs := $(addprefix ./,$(srcs))
+objs := $(patsubst ./%.cc,./%.o,$(srcs))
+
+test: out
+
+out: $(objs)
+
+$(objs): ./%.o: ./%.cc
+	echo $@: $<: $^
+
+a.o: a.cc a.h
+b.o: b.cc a.h b.h
+c.o: b.cc a.h b.h c.h
+
