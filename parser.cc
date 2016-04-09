89f6a2ba8c2844db8ff5b539df469c2239568874
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-04-09 12:32:23 +0900

    [C++] Error for missing endef
    A fix for https://github.com/google/kati/issues/55

--- parser.cc
+++ parser.cc
@@ -90,6 +90,9 @@ class Parser {
 
     if (!if_stack_.empty())
       ERROR("%s:%d: *** missing `endif'.", loc_.filename, loc_.lineno + 1);
+    if (!define_name_.empty())
+      ERROR("%s:%d: *** missing `endef', unterminated `define'.",
+            loc_.filename, define_start_line_);
   }
 
   static void Init() {
---
+++ testcase/err_missing_endef.mk
@@ -0,0 +1,4 @@
+all:
+	echo FAIL
+define foo
+xx
