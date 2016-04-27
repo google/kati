6823600be4c13b50bc8410d1192e9a048d5bfb8c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-04-27 15:47:57 +0900

    Add a test case to override_override.mk
    This is a testcase for 29b9b7470cc33c2b7c4264f254335d788ef04c26

--- testcase/override_override.mk
+++ testcase/override_override.mk
@@ -6,8 +6,14 @@ PASS_B
 endef
 B:=FAIL_B
 
+override C := FAIL_C
+override C := PASS_C
+C := FAIL_C2
+
 test:
 	echo $(A)
 	echo $(origin A)
 	echo $(B)
 	echo $(origin B)
+	echo $(C)
+	echo $(origin C)
