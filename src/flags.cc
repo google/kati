ae627d5c173fec619bfee4d8e0a612f04ff08837
KOLANICH <kolan_n@mail.ru>
2020-02-10 16:36:23 +0300

    Added printing the help print by CLI11 without actually depending on CLI11

--- src/flags.cc
+++ src/flags.cc
@@ -18,9 +18,14 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
 
 #include "log.h"
 #include "strutil.h"
+
+constexpr const auto helpMessage =
+#include "help.inc"
+    ;
 
 Flags g_flags;
 
@@ -59,6 +64,12 @@ void Flags::Parse(int argc, char** argv) {
       if (!HasPrefix(tok, "-") && tok.find('=') != string::npos)
         cl_vars.push_back(tok);
     }
+  }
+
+  if (argc >= 2 && (!strcmp(argv[1], "-help") || !strcmp(argv[1], "--help") ||
+                    !strcmp(argv[1], "-h"))) {
+    std::cout << helpMessage << std::endl;
+    return;
   }
 
   for (int i = 1; i < argc; i++) {
