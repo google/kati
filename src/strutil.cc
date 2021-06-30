google/kati

commit a8ca5d35306a48a81de9939925327de2f948c5eb

strutil: replace SSE4 specialization with libc call
SkipUntil had been partially specialized to use SSE4.2 instructions if
selected at compile time. The Makefile.ckati sets the compile flags to
the rather incompatible -march=native to enable the SSE4.2 instruction
set. The minimum (Intel) architecture required for SSE4.2 is Nehalem, so
-march=nehalem would do and that would probably make things a bit more
compatible when distributing the binary across machines.

It turns out, the relevant part of SkipUntil can actually be replaced by
a checked `strpbrk` call. Recent libc implementations do specialize
strpbrk for SSE4.2 as well. E.g. glibc runtime-selects `__strpbrk_sse42`
if the host provides the instruction set.

The ckati benchmark `strutil_bench` is neutral to this change. On my
machine the benchmark consistently produces around
   *kati*: WordScanner: 0.156697
with or without the change applied. This is when using the Make based
building process that also sets -march=native.

Other build systems and processes might not want to use -march=native to
be able to produce compatible builds. They might also not want to use
any limiting -march=* at all. That is the case for the Android build
where ckati is built and mainly used. When building in that environment,
the strutil_bench consistently runs
   *kati*: WordScanner: 0.396514
and when applying this patch
   *kati*: WordScanner: 0.255683

The difference between the best Android build and the Make based build
must be related to other build settings done in the Android/Soong based
build, such as FORTIFY_SOURCE, stack protector, etc.

Another side effect of this patch is, that we can run the kati test
suite successfully with --fsanitize=address now. Otherwise the SSE4.2
specific implementation would cause an address violation due to an
out-of-bounds read.

Signed-off-by: Matthias Maennich <maennich@google.com>

src/strutil.cc
@@ -25,48 +25,18 @@
 #include <stack>
 #include <utility>
 
-#ifdef __SSE4_2__
-#include <smmintrin.h>
-#endif
-
 #include "log.h"
 
 static bool isSpace(char c) {
   return (9 <= c && c <= 13) || c == 32;
 }
 
-#ifdef __SSE4_2__
-static int SkipUntilSSE42(const char* s,
-                          int len,
-                          const char* ranges,
-                          int ranges_size) {
-  __m128i ranges16 = _mm_loadu_si128((const __m128i*)ranges);
-  len &= ~15;
-  int i = 0;
-  while (i < len) {
-    __m128i b16 = _mm_loadu_si128((const __m128i*)(s + i));
-    int r = _mm_cmpestri(
-        ranges16, ranges_size, b16, len - i,
-        _SIDD_LEAST_SIGNIFICANT | _SIDD_CMP_RANGES | _SIDD_UBYTE_OPS);
-    if (r != 16) {
-      return i + r;
-    }
-    i += 16;
-  }
-  return len;
-}
-#endif
-
 template <typename Cond>
-static int SkipUntil(const char* s,
-                     int len,
-                     const char* ranges UNUSED,
-                     int ranges_size UNUSED,
-                     Cond cond) {
+static int SkipUntil(const char* s, int len, const char* ranges, Cond cond) {
   int i = 0;
-#ifdef __SSE4_2__
-  i += SkipUntilSSE42(s, len, ranges, ranges_size);
-#endif
+  const char* off = strpbrk(s, ranges);
+  if (off)
+    i += (off - s);
   for (; i < len; i++) {
     if (cond(s[i]))
       break;
@@ -90,7 +60,7 @@ WordScanner::Iterator& WordScanner::Iterator::operator++() {
   static const char ranges[] = "\x09\x0d  ";
   // It's intentional we are not using isSpace here. It seems with
   // lambda the compiler generates better code.
-  i = s + SkipUntil(in->data() + s, len - s, ranges, 4,
+  i = s + SkipUntil(in->data() + s, len - s, ranges,
                     [](char c) { return (9 <= c && c <= 13) || c == 32; });
   return *this;
 }
@@ -439,7 +409,7 @@ size_t FindThreeOutsideParen(StringPiece s, char c1, char c2, char c3) {
 size_t FindEndOfLine(StringPiece s, size_t e, size_t* lf_cnt) {
   static const char ranges[] = "\0\0\n\n\\\\";
   while (e < s.size()) {
-    e += SkipUntil(s.data() + e, s.size() - e, ranges, 6,
+    e += SkipUntil(s.data() + e, s.size() - e, ranges,
                    [](char c) { return c == 0 || c == '\n' || c == '\\'; });
     if (e >= s.size()) {
       CHECK(s.size() == e);
@@ -531,7 +501,7 @@ static bool NeedsShellEscape(char c) {
 void EscapeShell(string* s) {
   static const char ranges[] = "\0\0\"\"$$\\\\``";
   size_t prev = 0;
-  size_t i = SkipUntil(s->c_str(), s->size(), ranges, 10, NeedsShellEscape);
+  size_t i = SkipUntil(s->c_str(), s->size(), ranges, NeedsShellEscape);
   if (i == s->size())
     return;
 
@@ -549,7 +519,7 @@ void EscapeShell(string* s) {
     r += c;
     i++;
     prev = i;
-    i += SkipUntil(s->c_str() + i, s->size() - i, ranges, 10, NeedsShellEscape);
+    i += SkipUntil(s->c_str() + i, s->size() - i, ranges, NeedsShellEscape);
   }
   StringPiece(*s).substr(prev).AppendToString(&r);
   s->swap(r);

commit b12032b6d9865361be4741535e2059c9d1af6414

strutil: fix delimiters for skipping whitespaces
Commit a8ca5d35306a ("strutil: replace SSE4 specialization with libc
call") switched an SSE4 optimization of SkipUntil to using strpbrk. By
not matching the full ranges (i.e. not all control characters between
\x09 and \x0d), this lead to wrong results when processing real-world
Makefiles containing line continuation statements. Fix this by filling
up the set of delimiters.

Reported-by: Dan Willemsen <dwillemsen@google.com>
Fixes: a8ca5d35306a ("strutil: replace SSE4 specialization with libc call")
Signed-off-by: Matthias Maennich <maennich@google.com>

src/strutil.cc
@@ -60,7 +60,7 @@ WordScanner::Iterator& WordScanner::Iterator::operator++() {
     return *this;
   }
 
-  static const char delimiters[] = "\x09\x0d  ";
+  static const char delimiters[] = "\x09\x0a\x0b\x0c\x0d ";
   // It's intentional we are not using isSpace here. It seems with
   // lambda the compiler generates better code.
   i = s + SkipUntil(in->data() + s, len - s, delimiters,