google/kati

commit bca0a69ffd7045e33bd2e42e795cc0ade6f81c69

Remove threadpool implementation
The thread pool implementation is currently used for two cases:
1) parallel generation of the ninja output file content
2) second check whether regeneration is necessary

None of those are really on the hot path as rather the whole Eval
machinery is dominating the overall runtime.

There are a couple of issues with the current implementation. The thread
pool always spawns num_jobs threads whenever constructed. Even for the
use case 2) where there are two tasks running in the pool, as many
threads as cores are on the machine are usually spawned, wasting system
resources. In particular, use case 2) can be accomplished with running
the tasks side by side with std::async.

For the use case 1), what we effectively do is map-reduce a massive
string. Each of the tasks produces a substring of the final output and
once they are all generated (in memory), we dump them sorted into the
output file. This makes us build up the output in memory (compute and
memory) and defer the actual writing (i/o) to a later time.

The implementation is changed here to entirely skip using a thread pool.
Instead, we construct a buffered output file stream and pass it around
while we loop over the nodes. That way we can write out the file while
we are still producing the output having and can operate on a single
thread. This also reduces the overall memory foot print.

Those are the measured times from a recent Android aosp/master generator
run:

Without this change:

  (46.543s real, 46.24s user, 7.571s system, 5622MB maxrss)
  (45.176s real, 46.96s user, 5.268s system, 5622MB maxrss)
  (45.118s real, 46.94s user, 5.248s system, 5629MB maxrss)

With this change:

  (45.996s real, 42.05s user, 4.034s system, 4786MB maxrss)
  (45.727s real, 41.69s user, 4.129s system, 4789MB maxrss)
  (45.926s real, 42.19s user, 3.835s system, 4787MB maxrss)

As can be seen, the runtime difference is neglectible, but system

src/ninja.cc
@@ -21,7 +21,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <fstream>
 #include <map>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
@@ -41,7 +43,6 @@
 #include "string_piece.h"
 #include "stringprintf.h"
 #include "strutil.h"
-#include "thread_pool.h"
 #include "timeutil.h"
 #include "var.h"
 #include "version.h"
@@ -179,7 +180,6 @@ class NinjaGenerator {
   NinjaGenerator(Evaluator* ev, double start_time)
       : ce_(ev),
         ev_(ev),
-        fp_(NULL),
         rule_id_(0),
         start_time_(start_time),
         default_target_(NULL) {
@@ -473,16 +473,16 @@ class NinjaGenerator {
     return result;
   }
 
-  void EmitDepfile(NinjaNode* nn, string* cmd_buf, ostringstream* o) {
+  void EmitDepfile(NinjaNode* nn, string* cmd_buf, std::ostream& out) {
     const DepNode* node = nn->node;
     string depfile;
     if (!GetDepfile(node, cmd_buf, &depfile))
       return;
-    *o << " depfile = " << depfile << "\n";
-    *o << " deps = gcc\n";
+    out << " depfile = " << depfile << "\n";
+    out << " deps = gcc\n";
   }
 
-  void EmitNode(NinjaNode* nn, ostringstream* o) {
+  void EmitNode(NinjaNode* nn, std::ostream& out) {
     const DepNode* node = nn->node;
     const vector<Command*>& commands = nn->commands;
 
@@ -492,37 +492,37 @@ class NinjaGenerator {
       return;
     }
     if (g_flags.enable_debug) {
-      *o << "# " << (node->loc.filename ? node->loc.filename : "(null)") << ':'
-         << node->loc.lineno << "\n";
+      out << "# " << (node->loc.filename ? node->loc.filename : "(null)") << ':'
+          << node->loc.lineno << "\n";
     }
     if (!commands.empty()) {
       rule_name = StringPrintf("rule%d", nn->rule_id);
-      *o << "rule " << rule_name << "\n";
+      out << "rule " << rule_name << "\n";
 
       string description = "build $out";
       string cmd_buf;
       use_local_pool |= GenShellScript(node->output.c_str(), commands, &cmd_buf,
                                        &description);
-      *o << " description = " << description << "\n";
-      EmitDepfile(nn, &cmd_buf, o);
+      out << " description = " << description << "\n";
+      EmitDepfile(nn, &cmd_buf, out);
 
       // It seems Linux is OK with ~130kB and Mac's limit is ~250kB.
       // TODO: Find this number automatically.
       if (cmd_buf.size() > 100 * 1000) {
-        *o << " rspfile = $out.rsp\n";
-        *o << " rspfile_content = " << cmd_buf << "\n";
-        *o << " command = " << shell_ << " $out.rsp\n";
+        out << " rspfile = $out.rsp\n";
+        out << " rspfile_content = " << cmd_buf << "\n";
+        out << " command = " << shell_ << " $out.rsp\n";
       } else {
         EscapeShell(&cmd_buf);
-        *o << " command = " << shell_ << ' ' << shell_flags_ << " \"" << cmd_buf
-           << "\"\n";
+        out << " command = " << shell_ << ' ' << shell_flags_ << " \""
+            << cmd_buf << "\"\n";
       }
       if (node->is_restat) {
-        *o << " restat = 1\n";
+        out << " restat = 1\n";
       }
     }
 
-    EmitBuild(nn, rule_name, use_local_pool, o);
+    EmitBuild(nn, rule_name, use_local_pool, out);
   }
 
   string EscapeNinja(const string& s) const {
@@ -548,45 +548,45 @@ class NinjaGenerator {
   void EmitBuild(NinjaNode* nn,
                  const string& rule_name,
                  bool use_local_pool,
-                 ostringstream* o) {
+                 std::ostream& out) {
     const DepNode* node = nn->node;
     string target = EscapeBuildTarget(node->output);
-    *o << "build " << target;
+    out << "build " << target;
     if (!node->implicit_outputs.empty()) {
-      *o << " |";
+      out << " |";
       for (Symbol output : node->implicit_outputs) {
-        *o << " " << EscapeBuildTarget(output);
+        out << " " << EscapeBuildTarget(output);
       }
     }
-    *o << ": " << rule_name;
+    out << ": " << rule_name;
     vector<Symbol> order_onlys;
     if (node->is_phony && !g_flags.use_ninja_phony_output) {
-      *o << " _kati_always_build_";
+      out << " _kati_always_build_";
     }
     for (auto const& d : node->deps) {
-      *o << " " << EscapeBuildTarget(d.first).c_str();
+      out << " " << EscapeBuildTarget(d.first).c_str();
     }
     if (!node->order_onlys.empty()) {
-      *o << " ||";
+      out << " ||";
       for (auto const& d : node->order_onlys) {
-        *o << " " << EscapeBuildTarget(d.first).c_str();
+        out << " " << EscapeBuildTarget(d.first).c_str();
       }
     }
     if (!node->validations.empty()) {
-      *o << " |@";
+      out << " |@";
       for (auto const& d : node->validations) {
-        *o << " " << EscapeBuildTarget(d.first).c_str();
+        out << " " << EscapeBuildTarget(d.first).c_str();
       }
     }
 
-    *o << "\n";
+    out << "\n";
 
     if (!node->symlink_outputs.empty()) {
-      *o << " symlink_outputs =";
+      out << " symlink_outputs =";
       for (auto const& s : node->symlink_outputs) {
-        *o << " " << EscapeBuildTarget(s);
+        out << " " << EscapeBuildTarget(s);
       }
-      *o << "\n";
+      out << "\n";
     }
 
     string pool;
@@ -596,15 +596,15 @@ class NinjaGenerator {
 
     if (pool != "") {
       if (pool != "none") {
-        *o << " pool = " << pool << "\n";
+        out << " pool = " << pool << "\n";
       }
     } else if (g_flags.default_pool && rule_name != "phony") {
-      *o << " pool = " << g_flags.default_pool << "\n";
+      out << " pool = " << g_flags.default_pool << "\n";
     } else if (use_local_pool) {
-      *o << " pool = local_pool\n";
+      out << " pool = local_pool\n";
     }
     if (node->is_phony && g_flags.use_ninja_phony_output) {
-      *o << " phony_output = true\n";
+      out << " phony_output = true\n";
     }
     if (node->is_default_target) {
       unique_lock<mutex> lock(mu_);
@@ -616,53 +616,36 @@ class NinjaGenerator {
 
   void GenerateNinja() {
     ScopedTimeReporter tr("ninja gen (emit)");
-    fp_ = fopen(GetNinjaFilename().c_str(), "wb");
-    if (fp_ == NULL)
+    std::ofstream out(GetNinjaFilename(), std::ios::binary);
+    if (!out)
       PERROR("fopen(build.ninja) failed");
 
-    fprintf(fp_, "# Generated by kati %s\n", kGitVersion);
-    fprintf(fp_, "\n");
+    out << "# Generated by kati " << kGitVersion << "\n\n";
 
     if (!used_envs_.empty()) {
-      fprintf(fp_, "# Environment variables used:\n");
+      out << "# Environment variables used:\n";
       for (const auto& p : used_envs_) {
-        fprintf(fp_, "# %s=%s\n", p.first.c_str(), p.second.c_str());
+        out << "# " << p.first << "=" << p.second << '\n';
       }
-      fprintf(fp_, "\n");
+      out << '\n';
     }
 
     if (!g_flags.no_ninja_prelude) {
       if (g_flags.ninja_dir) {
-        fprintf(fp_, "builddir = %s\n\n", g_flags.ninja_dir);
+        out << "builddir = " << g_flags.ninja_dir << "\n\n";
       }
 
-      fprintf(fp_, "pool local_pool\n");
-      fprintf(fp_, " depth = %d\n\n", g_flags.num_jobs);
+      out << "pool local_pool\n"
+          << " depth = " << g_flags.num_jobs << "\n\n";
 
       if (!g_flags.use_ninja_phony_output) {
-        fprintf(fp_, "build _kati_always_build_: phony\n\n");
+        out << "build _kati_always_build_: phony\n\n";
       }
     }
 
-    unique_ptr<ThreadPool> tp(NewThreadPool(g_flags.num_jobs));
-    CHECK(g_flags.num_jobs);
-    int num_nodes_per_task = nodes_.size() / (g_flags.num_jobs * 10) + 1;
-    int num_tasks = nodes_.size() / num_nodes_per_task + 1;
-    vector<ostringstream> bufs(num_tasks);
-    for (int i = 0; i < num_tasks; i++) {
-      tp->Submit([this, i, num_nodes_per_task, &bufs]() {
-        int l =
-            min(num_nodes_per_task * (i + 1), static_cast<int>(nodes_.size()));
-        for (int j = num_nodes_per_task * i; j < l; j++) {
-          EmitNode(nodes_[j], &bufs[i]);
-        }
-      });
-    }
-    tp->Wait();
-
     if (!g_flags.generate_empty_ninja) {
-      for (const ostringstream& buf : bufs) {
-        fprintf(fp_, "%s", buf.str().c_str());
+      for (const auto& node : nodes_) {
+        EmitNode(node, out);
       }
     }
 
@@ -686,11 +669,9 @@ class NinjaGenerator {
       }
     }
     if (!g_flags.generate_empty_ninja) {
-      fprintf(fp_, "\n");
-      fprintf(fp_, "default %s\n", default_targets.c_str());
+      out << "\n"
+          << "default " << default_targets << '\n';
     }
-
-    fclose(fp_);
   }
 
   void GenerateShell() {
@@ -825,7 +806,6 @@ class NinjaGenerator {
 
   CommandEvaluator ce_;
   Evaluator* ev_;
-  FILE* fp_;
   SymbolSet done_;
   int rule_id_;
   bool use_goma_;

Makefile.ckati
@@ -47,7 +47,6 @@ KATI_CXX_SRCS := \
 	stringprintf.cc \
 	strutil.cc \
 	symtab.cc \
-	thread_pool.cc \
 	timeutil.cc \
 	var.cc
 

src/Android.bp
@@ -56,7 +56,6 @@ cc_library_host_static {
         "stringprintf.cc",
         "strutil.cc",
         "symtab.cc",
-        "thread_pool.cc",
         "timeutil.cc",
         "var.cc",
         "version_unknown.cc",

src/regen.cc
@@ -19,6 +19,7 @@
 #include <sys/stat.h>
 
 #include <algorithm>
+#include <future>
 #include <memory>
 #include <mutex>
 #include <vector>
@@ -32,7 +33,6 @@
 #include "ninja.h"
 #include "stats.h"
 #include "strutil.h"
-#include "thread_pool.h"
 
 namespace {
 
@@ -430,9 +430,7 @@ class StampChecker {
   }
 
   bool CheckStep2() {
-    unique_ptr<ThreadPool> tp(NewThreadPool(g_flags.num_jobs));
-
-    tp->Submit([this]() {
+    auto glob_future = std::async([this]() {
       string err;
       // TODO: Make glob cache thread safe and create a task for each glob.
       SetAffinityForSingleThread();
@@ -448,7 +446,7 @@ class StampChecker {
       }
     });
 
-    tp->Submit([this]() {
+    auto shell_future = std::async([this]() {
       SetAffinityForSingleThread();
       for (ShellResult* sr : commands_) {
         string err;
@@ -462,7 +460,8 @@ class StampChecker {
       }
     });
 
-    tp->Wait();
+    glob_future.wait();
+    shell_future.wait();
     if (needs_regen_) {
       fprintf(stderr, "%s", msg_.c_str());
     }

src/thread_pool.cc
@@ -1,90 +1,0 @@
-// Copyright 2016 Google Inc. All rights reserved
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build ignore
-
-#include "thread_pool.h"
-
-#include <condition_variable>
-#include <mutex>
-#include <stack>
-#include <thread>
-#include <vector>
-
-#include "affinity.h"
-
-class ThreadPoolImpl : public ThreadPool {
- public:
-  explicit ThreadPoolImpl(int num_threads) : is_waiting_(false) {
-    SetAffinityForMultiThread();
-    threads_.reserve(num_threads);
-    for (int i = 0; i < num_threads; i++) {
-      threads_.push_back(thread([this]() { Loop(); }));
-    }
-  }
-
-  virtual ~ThreadPoolImpl() override {}
-
-  virtual void Submit(function<void(void)> task) override {
-    unique_lock<mutex> lock(mu_);
-    tasks_.push(task);
-    cond_.notify_one();
-  }
-
-  virtual void Wait() override {
-    {
-      unique_lock<mutex> lock(mu_);
-      is_waiting_ = true;
-      cond_.notify_all();
-    }
-
-    for (thread& th : threads_) {
-      th.join();
-    }
-
-    SetAffinityForSingleThread();
-  }
-
- private:
-  void Loop() {
-    while (true) {
-      function<void(void)> task;
-      {
-        unique_lock<mutex> lock(mu_);
-        if (tasks_.empty()) {
-          if (is_waiting_)
-            return;
-          cond_.wait(lock);
-        }
-
-        if (tasks_.empty())
-          continue;
-
-        task = tasks_.top();
-        tasks_.pop();
-      }
-      task();
-    }
-  }
-
-  vector<thread> threads_;
-  mutex mu_;
-  condition_variable cond_;
-  stack<function<void(void)>> tasks_;
-  bool is_waiting_;
-};
-
-ThreadPool* NewThreadPool(int num_threads) {
-  return new ThreadPoolImpl(num_threads);
-}

src/thread_pool.h
@@ -1,35 +1,0 @@
-// Copyright 2016 Google Inc. All rights reserved
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#ifndef THREAD_POOL_H_
-#define THREAD_POOL_H_
-
-#include <functional>
-
-using namespace std;
-
-class ThreadPool {
- public:
-  virtual ~ThreadPool() = default;
-
-  virtual void Submit(function<void(void)> task) = 0;
-  virtual void Wait() = 0;
-
- protected:
-  ThreadPool() = default;
-};
-
-ThreadPool* NewThreadPool(int num_threads);
-
-#endif  // THREAD_POOL_H_

commit 108a81ad98e950efdf3491cc07ed662a0dcb8c8e

Fix crash when using --empty_ninja_file
This bug was introduced in commit bca0a69ffd7045e33bd2e42e795cc0ade6f81c69.
EmitNode() used to always be called, but after that commit, EmitNode()
is no longer called when using --empty_ninja_file. EmitNode() has the side
effect of setting default_target_, which was then needed later on.

However it turns out that default_target_ is not actually needed when
using --empty_ninja_file, so moving the code that uses it into the check
that that flag is not provided fixes the issue.

src/ninja.cc
@@ -645,6 +645,20 @@ class NinjaGenerator {
       for (const auto& node : nodes_) {
         EmitNode(node, out);
       }
+
+      std::string default_targets;
+      if (g_flags.targets.empty() || g_flags.gen_all_targets) {
+        CHECK(default_target_);
+        default_targets = EscapeBuildTarget(default_target_->output);
+      } else {
+        for (Symbol s : g_flags.targets) {
+          if (!default_targets.empty())
+            default_targets += ' ';
+          default_targets += EscapeBuildTarget(s);
+        }
+      }
+      out << "\n"
+          << "default " << default_targets << '\n';
     }
 
     SymbolSet used_env_vars(Vars::used_env_vars());
@@ -653,22 +667,6 @@ class NinjaGenerator {
     for (Symbol e : used_env_vars) {
       StringPiece val(getenv(e.c_str()));
       used_envs_.emplace(e.str(), val.as_string());
-    }
-
-    std::string default_targets;
-    if (g_flags.targets.empty() || g_flags.gen_all_targets) {
-      CHECK(default_target_);
-      default_targets = EscapeBuildTarget(default_target_->output);
-    } else {
-      for (Symbol s : g_flags.targets) {
-        if (!default_targets.empty())
-          default_targets += ' ';
-        default_targets += EscapeBuildTarget(s);
-      }
-    }
-    if (!g_flags.generate_empty_ninja) {
-      out << "\n"
-          << "default " << default_targets << '\n';
     }
   }
 

testcase/empty_ninja_file.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright 2021 Google Inc. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This just tests that kati doesn't crash when using --empty_ninja_file
+
+set -u
+
+mk="$@"
+
+cat <<EOF > Makefile
+result:
+	@echo "Foo" > \$@
+EOF
+
+if echo "${mk}" | grep -qv "kati"; then
+  # Make doesn't support --empty_ninja_file
+  ${mk} --warn 2>&1
+else
+  ${mk} --ninja --empty_ninja_file --warn 2>&1
+fi